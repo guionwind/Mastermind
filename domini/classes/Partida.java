bcec5128a65a86b9704e4e0cdbfdf18d88d49b61
marc.ortiz.lopez <marc.ortiz.lopez@estudiantat.upc.edu>
2023-03-21 16:03:40 +0100

    He avançat les clases

--- domini/classes/Partida.java
+++ domini/classes/Partida.java
@@ -2,4 +2,8 @@ package domini.classes;
 
 public class Partida {
     private int id;
+
+    public int getId(){
+        return this.id;
+    }
 }
--- domini/classes/Ranquing.java
+++ domini/classes/Ranquing.java
@@ -2,5 +2,20 @@ package domini.classes;
 
 public class Ranquing {
     private int id;
-    private EstadistiquesPartida puntuacions[];
+    private EstadistiquesPartida estadistiques[]; //TODO això hauria d'estar ordenat sempre, de millor a pitjor
+
+    //mètodes
+    public int getId(){
+        return this.id;
+    }
+    public void isOnRanquing(EstadistiquesPartida estadistica){
+        int puntuacio = estadistica.getPuntuacio();
+        for (int i = 0; i < this.estadistiques.length; i++){
+            EstadistiquesPartida estad = estadistiques[i];
+            int tempPuntuacio = estad.getPuntuacio();
+            if (puntuacio >= tempPuntuacio){
+                //TODO afegir puntuacio a la llista
+            }
+        }
+    }
 }
