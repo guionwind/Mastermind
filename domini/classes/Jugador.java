705789bd19e2820bba260beb1b7cd2ee0432a384
sheng.ye <sheng.ye@estudiantat.upc.edu>
2023-03-21 16:02:13 +0100

    afegir-atributs-jugador

--- domini/classes/Jugador.java
+++ domini/classes/Jugador.java
@@ -1,3 +1,13 @@
 public class Jugador {
+    //atributs
+    private int id;
+    private String username;
+    private String password;
+    private int partidesGuanyades;
+    private int partidesJugades;
+
+    //associacions
+    private ConfiguracioGlobal config;
+    private Partida[] partides;
     
 }
