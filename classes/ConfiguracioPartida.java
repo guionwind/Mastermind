c265dc5e3dd803980ed6be51349868161cdb0d39
david.barranquero <david.barranquero@estudiantat.upc.edu>
2023-03-21 12:31:39 +0100

    Creació classes FiveGuess, ConfiguracioPartida i la interface Maquina

---
+++ classes/ConfiguracioPartida.java
@@ -0,0 +1,15 @@
+//package Domini;
+// import Dominio.Persona;
+
+public class ConfiguracioPartida {
+    // Atributs
+
+    TipusPartida tipusPartida; // ENUMS?
+    Integer numeroIntents;
+    Integer numeroColors;
+    Integer longitudCombinacio;
+
+
+    // Metodes
+
+}
---
+++ classes/FiveGuess.java
@@ -0,0 +1,18 @@
+//package Domini;
+
+public class FiveGuess implements Maquina {
+    // Atributs
+
+
+    // Metodes
+
+    /*
+    Given the solution code, the solve operation uses one of the proposed algorithms
+    (either five guess or the genetic one) to create the list of codes that will lead
+    to the solution. If the algorithm is unable to find the solution in less than
+    maxSteps steps, the returned list will contain a list composed of maxSteps codes.
+    */
+    public List<List<Integer>>  solve (List<Integer> solution); {
+
+    }
+}
