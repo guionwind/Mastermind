fa603070e9b998e2611f76f32a910fae46b8a02f
arnau.domenech <arnau.domenech@estudiantat.upc.edu>
2023-03-28 14:24:30 +0200

    refactor(Partida)

--- domini/classes/Partida.java
+++ domini/classes/Partida.java
@@ -1,5 +1,21 @@
 package domini.classes;
 
-public class Partida {
+public abstract class Partida {
     private int id;
+    private int solutionCode;
+
+
+    public Partida(int id, int solutionCode) {
+        this.id = id;
+        this.solutionCode = solutionCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSolutionCode() {
+        return solutionCode;
+    }
+
 }
