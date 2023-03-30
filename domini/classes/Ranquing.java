7cbe7bdf882c1ed7876cad5c11c46988ecdacc0e
marc.ortiz.lopez <marc.ortiz.lopez@estudiantat.upc.edu>
2023-03-30 15:44:27 +0200

    Edited ranquing

--- domini/classes/Ranquing.java
+++ domini/classes/Ranquing.java
@@ -1,21 +1,42 @@
 package domini.classes;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+class estadisticasComparator implements Comparator<EstadistiquesPartida> {
+
+    @Override
+    public int compare(EstadistiquesPartida first, EstadistiquesPartida second) {
+       return Integer.compare(first.getPuntuacio(), second.getPuntuacio());
+    }
+
+}
 
 public class Ranquing {
     private int id;
-    private EstadistiquesPartida estadistiques[]; //TODO això hauria d'estar ordenat sempre, de millor a pitjor
+    private ArrayList<EstadistiquesPartida> estadistiques = new ArrayList<EstadistiquesPartida>(); //TODO això hauria d'estar ordenat sempre, de millor a pitjor
 
     //mètodes
     public int getId(){
         return this.id;
     }
-    public void isOnRanquing(EstadistiquesPartida estadistica){
+    /*public void isOnRanquing(EstadistiquesPartida estadistica){
         int puntuacio = estadistica.getPuntuacio();
-        for (int i = 0; i < this.estadistiques.length; i++){
-            EstadistiquesPartida estad = estadistiques[i];
+        for (int i = 0; i < 10; i++){
+            EstadistiquesPartida estad = this.estadistiques.get(i);
             int tempPuntuacio = estad.getPuntuacio();
             if (puntuacio >= tempPuntuacio){
-                //TODO afegir puntuacio a la llista
+                this.estadistiques.add(i, estadistica);
+                if (this.estadistiques.size() > 10){
+                    this.estadistiques.remove(10);
+                }
             }
         }
+    }*/
+    public void add(EstadistiquesPartida estadisitica){
+        this.estadistiques.add(estadisitica);
+        estadisticasComparator estatComparator = new estadisticasComparator();
+        Collections.sort(this.estadistiques, estatComparator);
     }
 }
