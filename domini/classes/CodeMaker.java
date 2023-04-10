b7e387dace5ba3aa43662503a065bf7e3e6f27c7
arnau.domenech <arnau.domenech@estudiantat.upc.edu>
2023-04-10 20:24:01 +0200

    refactor(Partida,CodeBreaker i CodeMaker)

---
+++ domini/classes/CodeMaker.java
@@ -0,0 +1,8 @@
+package domini.classes;
+
+public class CodeMaker extends Partida {
+    public CodeMaker(int id, EstadistiquesPartida estadistiquesPartida, int solutionCode) {
+        super(id, estadistiquesPartida, solutionCode);
+    }
+
+}
