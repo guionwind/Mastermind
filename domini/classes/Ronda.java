2e999e51e1309e7397dc69c2aa26d0682b731e53
marc.ortiz.lopez <marc.ortiz.lopez@estudiantat.upc.edu>
2023-04-11 13:03:21 +0200

    Working on Ronda.java

--- domini/classes/Ronda.java
+++ domini/classes/Ronda.java
@@ -3,4 +3,21 @@ package domini.classes;
 public class Ronda {
     private int id;
     private String combinacioIntentada;
+    /*creadora per defecte. 
+    * crea una nova Ronda amb ID incremental (partida s´encarrega de que les IDs siguien incrementals) 
+    * despres assigna la combinacions intentades
+    * 
+    */
+    public Ronda(int id, String combinacioIntentada) {
+        this.id = id;
+        this.combinacioIntentada = combinacioIntentada;
+    }
+
+    //metodes
+    public int getId(){
+        return this.id;
+    }
+    public String getCombinacioIntentada(){
+        return this.combinacioIntentada;
+    }
 }
