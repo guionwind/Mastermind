2f5985da4aff465c2aa1b0c7e5427fa467310444
marc.ortiz.lopez <marc.ortiz.lopez@estudiantat.upc.edu>
2023-04-12 17:00:46 +0200

    fixed sintaxis

--- drivers/DriverRonda.java
+++ drivers/DriverRonda.java
@@ -6,18 +6,25 @@ public class DriverRonda {
     public void testConstructora(){
         Random randInt = new Random();
         int myRandomId = randInt.nextInt(20);
-        Ronda test = new Ronda(myRandomId, "1234");
+        Integer[] combinacioIntentada = {1,2,3,4};
+        Ronda test = new Ronda(myRandomId, combinacioIntentada);
         System.out.println("testConstructora: Creada una ronda test amb ID random: "+myRandomId+" / CombinacioIntentada: 1234");
     }
     public void testGetId(){
-        Ronda test = new Ronda(6, "2341");
+        Integer[] combinacioIntentada = {1,2,3,4};
+        Ronda test = new Ronda(6, combinacioIntentada);
         int id = test.getId();
         System.out.println("testGetId: Agafem Id: "+id);
     }
     public void testGetCombinacioIntentada(){
-        Ronda test = new Ronda(6, );
-        String combinacioIntentada = test.getCombinacioIntentada();
-        System.out.println("testGetCombinacioIntentada: Agafem Combinacio Intentada: "+combinacioIntentada);
+        Integer[] combinacioIntentada = {1,2,3,4};
+        Ronda test = new Ronda(6, combinacioIntentada);
+        Integer[] combinacioIntentada_1 = test.getCombinacioIntentada();
+        System.out.print("testGetCombinacioIntentada: Agafem Combinacio Intentada: ");
+        for (int i = 0; i < combinacioIntentada_1.length; i++) {
+            System.out.print(combinacioIntentada_1[i]);
+        }
+        System.out.println();
     }
     public static void main (String [] args){
         DriverRonda test = new DriverRonda();
