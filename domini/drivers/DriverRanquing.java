01bdf348e3b025a08c9a427be981b1227ac9aea8
marc.ortiz.lopez <marc.ortiz.lopez@estudiantat.upc.edu>
2023-04-12 12:23:06 +0200

    Done DriverRonda and DriverRanquing

--- domini/drivers/DriverRanquing.java
+++ domini/drivers/DriverRanquing.java
@@ -1,5 +1,48 @@
 package domini.drivers;
+import domini.classes.*;
+
+import java.util.ArrayList;
 
 public class DriverRanquing {
-    
+    public void testConstructora(){
+        Ranquing test = new Ranquing();
+        Ranquing test2 = new Ranquing();
+        System.out.println("testConstructora: Creat una ranquing test amb Id: "+test.getId()+" i un altre amb Id: "+test2.getId());
+    }
+    public void testGetId(){
+        Ranquing test = new Ranquing();
+        int id = test.getId();
+        System.out.println("testGetId: Agafem Id: "+id);
+    }
+    public void testAdd(){
+        Ranquing test = new Ranquing();
+        Jugador jugador = new Jugador("Pepe", "1234");
+        EstadistiquesPartida estadistica = new EstadistiquesPartida(jugador, null, 2300, 300);
+        test.add(estadistica);
+        System.out.println("testAdd: Afegim estadistica amb jugador Pepe, 2300 de puntuacio, partida nula i temps 300");
+    }
+    public void testGetTopN(){
+        Ranquing test = new Ranquing();
+        Jugador jugador = new Jugador("Pepe", "1234");
+        EstadistiquesPartida estadistica = new EstadistiquesPartida(jugador, null, 5000, 300);
+        test.add(estadistica);
+        Jugador jugador_2 = new Jugador("Maria", "1234");
+        EstadistiquesPartida estadistica_2 = new EstadistiquesPartida(jugador_2, null, 1000, 500);
+        test.add(estadistica_2);
+        Jugador jugador_3 = new Jugador("Antonio", "1234");
+        EstadistiquesPartida estadistica_3 = new EstadistiquesPartida(jugador_3, null, 2300, 400);
+        test.add(estadistica_3);
+        System.out.println("testGetTopN: Dels 3 Jugadors (Pepe: 5000, Maria: 1000, Antonio: 2300) llistem el TOP 2: ");
+        ArrayList<EstadistiquesPartida> top_2 = test.getTopN(2);
+        for (int i = 0; i < top_2.size(); i++){
+            System.out.println((i+1)+": "+ top_2.get(i).getJugador().getUsername() + "("+ top_2.get(i).getPuntuacio()+" punts)");
+        }
+    }
+    public static void main (String [] args){
+        DriverRanquing test = new DriverRanquing();
+        test.testConstructora();
+        test.testGetId();
+        test.testAdd();
+        test.testGetTopN();
+    }
 }
--- domini/drivers/DriverRonda.java
+++ domini/drivers/DriverRonda.java
@@ -1,5 +1,28 @@
 package domini.drivers;
+import domini.classes.Ronda;
+import java.util.Random;
 
 public class DriverRonda {
-    
+    public void testConstructora(){
+        Random randInt = new Random();
+        int myRandomId = randInt.nextInt(20);
+        Ronda test = new Ronda(myRandomId, "1234");
+        System.out.println("testConstructora: Creada una ronda test amb ID random: "+myRandomId+" / CombinacioIntentada: 1234");
+    }
+    public void testGetId(){
+        Ronda test = new Ronda(6, "2341");
+        int id = test.getId();
+        System.out.println("testGetId: Agafem Id: "+id);
+    }
+    public void testGetCombinacioIntentada(){
+        Ronda test = new Ronda(6, "2341");
+        String combinacioIntentada = test.getCombinacioIntentada();
+        System.out.println("testGetCombinacioIntentada: Agafem Combinacio Intentada: "+combinacioIntentada);
+    }
+    public static void main (String [] args){
+        DriverRonda test = new DriverRonda();
+        test.testConstructora();
+        test.testGetId();
+        test.testGetCombinacioIntentada();
+    }
 }
