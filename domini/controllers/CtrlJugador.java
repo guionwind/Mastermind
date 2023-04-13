7870feb90b4b14d6b1d7292f8cd4ba0d3923fba5
sheng.ye <sheng.ye@estudiantat.upc.edu>
2023-04-13 18:48:51 +0200

    wip-refactoring-CtrlJugador

--- domini/controllers/CtrlJugador.java
+++ domini/controllers/CtrlJugador.java
@@ -1,36 +1,45 @@
 package domini.controllers;
 
-import domini.classes.*;
+import domini.classes.Jugador;
+//per retocar
+import domini.classes.exceptions.*;
+
+import java.io.IOException;
 import java.lang.String;
-import java.util.Map;
+import java.util.HashMap;
+
+
 
 public class CtrlJugador {
-    private Jugador JugadorActual;
+    private int JugadorActual;
     //en crear un jugador, aqui guardarem les credencials
-    private Map <String, String> usuariContrasenya;
+    private HashMap <Integer, Jugador> jugadors;
     
     //constructora
+    //si no hi ha jugador, JugadorActual = -1
     public CtrlJugador() {
-        JugadorActual = null;
+        JugadorActual = -1;
     }
 
     //retorna el jugador actual
-    //sera null si no hi ha ningu logejat
-    public Jugador getJugadorActual() {
-        return JugadorActual;
+    //sera -1 si no hi ha ningu logejat
+    public int getIdJugador() throws {
+        if (JugadorActual != -1) return JugadorActual;
+        //else throw 
     }
 
     //crea un nou jugador per al registre, afegeix les credencials a usuariContrasenya
-    public void crearJugador(String username, String password) {
+    public void crearJugador(String username, String password) throws JugadorJaExisteix {
         if (JugadorActual == null) {
             new Jugador(username, password);
             usuariContrasenya.put(username, password);
         }
     }
 
-    //treu el JugadorActual, nomes si n'hi ha un actualment
+    //treu el JugadorActual
+    //
     public void logoff() {
-        if (JugadorActual != null) JugadorActual = null;
+        if (JugadorActual != -1) JugadorActual = -1;
     }
 
     //obte la contrasenya de l'usuari a partir del seu username
@@ -38,7 +47,9 @@ public class CtrlJugador {
         if (usuariContrasenya.containsKey(username)) {
             String pass = usuariContrasenya.get(username);
             return pass;
+        }
         //else throw Exception
     }
+
 
 }
