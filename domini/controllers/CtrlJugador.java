08659cc0bb89a34ba7e2a2a34b2aac986c17cd1d
sheng.ye <sheng.ye@estudiantat.upc.edu>
2023-04-14 18:20:08 +0200

    afegides excepcions de la creadora

--- domini/controllers/CtrlJugador.java
+++ domini/controllers/CtrlJugador.java
@@ -20,7 +20,6 @@ public class CtrlJugador {
     //si no hi ha jugador, JugadorActual = -1
     public CtrlJugador() {
         jugadorActual = -1;
-        jugadors = new HashMap <Integer, Jugador>();
         jugadors = new HashMap<Integer, Jugador>();
     }
 
@@ -40,18 +39,17 @@ public class CtrlJugador {
 
 
     //crea un nou jugador per al registre, afegeix les credencials a usuariContrasenya
-    //* no se'l logeja automaticament
+    //* se'l logeja automaticament
     public void crearJugador(String username, String password) throws JugadorJaExisteix, JugadorInvalid {
+        if (username.equals(null) && username.equals("")) throw new JugadorInvalid("No has afegit un username");
         if (jugadorActual == -1) {
             Jugador j = new Jugador(username, password);
             int newId = j.getID();
             jugadors.put(newId, j);
             jugadorActual = newId;
-        }else{
-            if (jugadorActual == -2) throw new JugadorJaExisteix("XD");
         }
         else {
-            throw new JugadorJaExisteix("matat");
+            throw new JugadorJaExisteix("Ja hi ha un jugador amb aquest nom");
         }
     }
 
