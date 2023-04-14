4ebce768e2874380e7e6cbf7ea000e7e089471a3
LordNetro <xdmarcitos@hotmail.com>
2023-04-14 23:51:26 +0200

    Final

--- FONTS/src/compiler.bat
+++ FONTS/src/compiler.bat
@@ -1,5 +1,13 @@
 @echo off
 echo Compilant projecte Mastermind...
-javac domini/controllers/CtrlPresentacio.java
+javac drivers/Driver.java
 echo Compilacio finalitzada.
+if "%1"=="jar" (
+    echo Creant arxiu JAR...
+    jar cmvf ./drivers/Driver.mf ../../EXE/Driver.jar -C ../../EXE/out/ .
+    echo Arxiu JAR creat.
+)
+if "%1"=="run" (
+    java drivers.Driver
+)
 pause
