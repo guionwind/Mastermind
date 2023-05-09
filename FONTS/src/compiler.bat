f8bcb601e7f1d1180696abaacca54f79504a5eb4
marc.ortiz.lopez <marc.ortiz.lopez@estudiantat.upc.edu>
2023-05-09 14:58:47 +0200

    minor changes

--- FONTS/src/compiler.bat
+++ FONTS/src/compiler.bat
@@ -1,13 +1,13 @@
 @echo off
 echo Compilant projecte Mastermind...
-javac controllers/CtrlPresentacio.java
+javac domini/controllers/CtrlPresentacio.java
 echo Compilacio finalitzada.
 if "%1"=="jar" (
     echo Creant arxiu JAR...
-    jar cmvf ./controllers/CtrlPresentacio.mf ../../EXE/CtrlPresentacio.jar -C ../../EXE/out/ .
+    jar cmvf ./domini/controllers/CtrlPresentacio.mf ../../EXE/CtrlPresentacio.jar -C ../../EXE/out/ .
     echo Arxiu JAR creat.
 )
 if "%1"=="run" (
-    java controllers.CtrlPresentacio
+    java domini.controllers.CtrlPresentacio
 )
 pause
