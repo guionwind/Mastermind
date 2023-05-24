84f717fcb12ef27c5056aae6cf9980a0e3e9971b
sheng.ye <sheng.ye@estudiantat.upc.edu>
2023-05-24 16:14:36 +0200

    refactor DAOEstadistiquesPartida

--- FONTS/src/persistencia/classes/DAOEstadistiquesPartida.java
+++ FONTS/src/persistencia/classes/DAOEstadistiquesPartida.java
@@ -3,19 +3,6 @@ package persistencia.classes;
 public class DAOEstadistiquesPartida {
 
     /**
-     * Identificador del jugador que ha jugat la partida.
-     * Part de la clau primària juntament amb idPartida.
-     */
-    private int idJugador;
-
-    /**
-     * Identificador de la partida de la qual s'agafen les estadistiques.
-     * Part de la clau primària juntament amb idJugador.
-     * La partida és de tipus Codebreaker.
-     */
-    private int idPartida;
-
-    /**
      * Puntuació obtinguda a la partida.
      */
     private int puntuacio;
@@ -34,33 +21,13 @@ public class DAOEstadistiquesPartida {
     /**
      * Constructora amb paràmetres.
      * 
-     * @param idJugador         Identificador del jugador
-     * @param idPartida         Identificador de la partida
      * @param puntuacio         Puntuacio obtinguda a la partida
      * @param guanyada          Si la partida ha estat guanyada, guanyada==true
      */
 
-    public DAOEstadistiquesPartida(int idJugador, int idPartida, int puntuacio, Boolean guanyada) {
-        this.idJugador = idJugador;
-        this.idPartida = idPartida;
+    public DAOEstadistiquesPartida(int puntuacio, Boolean guanyada) {
         this.puntuacio = puntuacio;
         this.guanyada = guanyada;
-    }
-
-    /**
-     * Retorna la id del jugador.
-     * @return id del jugador
-     */
-    public int getIdJugador() {
-        return idJugador;
-    }
-
-    /**
-     * Retorna la id de la partida.
-     * @return id de la partida
-     */
-    public int getIdPartida() {
-        return idPartida;
     }
 
     /**
@@ -75,7 +42,7 @@ public class DAOEstadistiquesPartida {
      * Retorna si la partida ha estat guanyada.
      * @return Cert si ha estat guanyada, fals si no.
      */
-    public Boolean guanyada() {
+    public Boolean getGuanyada() {
         return guanyada;
     }
 }
