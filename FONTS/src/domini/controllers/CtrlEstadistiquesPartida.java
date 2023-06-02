72645de7de1d6a9105a2ed6f5ae9963bb811c96a
david.barranquero <david.barranquero@estudiantat.upc.edu>
2023-06-02 19:25:33 +0200

    m

--- FONTS/src/domini/controllers/CtrlEstadistiquesPartida.java
+++ FONTS/src/domini/controllers/CtrlEstadistiquesPartida.java
@@ -1,27 +1,72 @@
 package domini.controllers;
 
+import java.util.*;
+
 import domini.classes.EstadistiquesPartida;
 import presentacio.custom.Pair;
-
-import java.awt.font.ImageGraphicAttribute;
-import java.util.HashMap;
-
-/**
+/*
  * Controlador que gestiona les estadistiques de les partides jugades
  */
 public class CtrlEstadistiquesPartida {
-    
-    private EstadistiquesPartida estadisticaPartidaActual;
+
+    private HashMap<Pair<String, Integer>, EstadistiquesPartida> estadistiquesPartida;
+
 
     public CtrlEstadistiquesPartida() {
-        this.estadisticaPartidaActual = null;
+        estadistiquesPartida = new HashMap<>();
     }
 
-    public void setEstadisticaActual(EstadistiquesPartida estadisticaPartidaActual) {
-        this.estadisticaPartidaActual = estadisticaPartidaActual;
+    /**
+     * Crea una nova instància d'EstadistiquesPartida i la retorna.
+     * 
+     * @param idJugador id del Jugador que ha jugat la partida
+     * @param idPartida id de la Partida corresponent
+     * @param puntuacio puntuacio obtinguda en la partida
+     * @param guanyada true si el jugador ha guanyat la partida, false si no
+     */
+    public void creaEstadistiquesPartida(String username, Integer idPartida, Integer puntuacio, Boolean guanyada) {
+        EstadistiquesPartida eP = new EstadistiquesPartida(username, idPartida, puntuacio, guanyada);
+
+        Pair<String, Integer> identificadors = new Pair<String, Integer>(username, idPartida);
+        estadistiquesPartida.put(identificadors ,eP);
     }
 
-    public EstadistiquesPartida getEstadisticaActual() {
-        return this.estadisticaPartidaActual;
+    //? potser les dues funcions d'aqui a sota s'haurien de fer a CtrlDomini
+    /**
+     * Retorna la puntuacio obtinguda en una partida donada
+     * @param idJugador Id del Jugador de la partida
+     * @param idPartida Id de la partida jugada
+     * @return Puntuacio obtinguda
+     */
+    public Integer getPuntuacio(String username, Integer idPartida) {
+        for (HashMap.Entry<Pair<String, Integer>, EstadistiquesPartida> estadisticaPartida : estadistiquesPartida.entrySet()) {
+            Pair<String, Integer> key = estadisticaPartida.getKey(); //Key = {idJugador, idPartida}
+
+            System.out.println("ANALITZANT partida : "+key.getSecond()+" jugador: "+key.getFirst());
+            if (key.getFirst() == username && key.getSecond() == idPartida){
+                EstadistiquesPartida ed = estadisticaPartida.getValue();
+                System.out.println("ESTIC A CTRLESTATICS: "+ed.getPuntuacio());
+                return ed.getPuntuacio();
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Obte una instancia d'estadistiquesPartida
+     * @param idJugador Id del Jugador de la partida
+     * @param idPartida Id de la partida jugada
+     * @return La instancia corresponent
+     */
+    public EstadistiquesPartida getEstadistiquesPartida(String username, Integer idPartida) {
+        for (HashMap.Entry<Pair<String, Integer>, EstadistiquesPartida> entry : estadistiquesPartida.entrySet()) {
+            Pair<String, Integer> key = entry.getKey();
+            EstadistiquesPartida ed = entry.getValue();
+            if (key.getSecond() == idPartida && key.getFirst() == username){
+                return ed;
+            }
+        }
+        return null;
     }
 }
